f22a99aee8d758df0c3a1266f0ba4f8375234104
evan-boissonnot <commercial@boissonnot.Fr>
2018-12-10 23:54:12 +0100

    mise à jour suivant regex

--- regex-route/routes/withRegex.js
+++ regex-route/routes/withRegex.js
@@ -10,6 +10,15 @@ router.get(/.*wookie$/, function(req, res) {
     res.send('wookie');
 });
 
-
+router.get(/article(\d{1,3})?\/?$/, function(req, res, next) {
+    var n;
+    if (req.params[0]) // param car grouping en regex
+      n = parseInt(req.params[0], 10);
+  
+    if (!n || (n > 0 && n < 900))
+      res.render('article' + (n ? n : ''));
+    else
+      next();
+  });
 
 module.exports = router;
