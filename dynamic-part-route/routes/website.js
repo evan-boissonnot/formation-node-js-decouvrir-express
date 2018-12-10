21d24c1194e1139cbbeef796882f63ba252bfc7c
evan-boissonnot <commercial@boissonnot.Fr>
2018-12-10 23:32:28 +0100

    mise à jour avec utilisation req.param

--- dynamic-part-route/routes/website.js
+++ dynamic-part-route/routes/website.js
@@ -8,4 +8,16 @@ router.get('/:directory/:view', function(req, res, next) {
     res.render(req.params.directory + '/' + req.params.view, { title: 'website' });
 });
 
+router.param("view", (req, res, next, value, name) => {
+    console.log(`view ? ${name}=>${value}`);
+
+    return next();
+});
+
+router.param("directory", (req, res, next, value, name) => {
+    console.log(`directory ? ${name}=>${value}`);
+
+    return next();
+});
+
 module.exports = router;
